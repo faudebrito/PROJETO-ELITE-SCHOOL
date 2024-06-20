095dc0bb059707c263776c1d789e4eb1331f1f8d
nathaliadev7 <natalia@gmail>
2024-06-20 15:19:06 -0300

    versao2

--- gerenciador_school/admin.py
+++ gerenciador_school/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Categoria_produtos,Produtos
 
 # Register your models here.
+@admin.register(Categoria_produtos)
+class AdminProdutos(admin.ModelAdmin):
+    ...
+
+@admin.register(Produtos)
+class AdminCategoria_produtos(admin.ModelAdmin):
+    ...
+
+# admin.site.register(Categoria_produtos, AdminCategoria_produtos)
--- gerenciador_school/models.py
+++ gerenciador_school/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Categoria_produtos(models.Model):
+    nome_categoria = models.CharField(max_length=45)
+
+    def __str__(self) -> str:
+        return self.nome_categoria
+
+class Produtos(models.Model):
+    nome_produto = models.CharField(max_length=40)
+    qtd_produto = models.IntegerField()
+    valor_produto = models.FloatField()
+    data_vencimento_produto = models.DateField(auto_now_add=True)
+    img_produto = models.ImageField(upload_to='gerenciador_school/images/%Y/%m/%d/')
+    categoria = models.ForeignKey(Categoria_produtos,on_delete=models.SET_NULL,null=True)
+    dono = models.ForeignKey(User,on_delete=models.SET_NULL,null=True)
+                                  
+    def __str__(self) -> str:
+        return self.nome_produto
